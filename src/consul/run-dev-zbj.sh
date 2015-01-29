6fe8458fa1d029756b40027ba869d8b035ae9b7d
Luo Tao <lotreal@gmail.com>
2015-01-29 06:00:34 +0000

    add consul boot script

---
+++ src/consul/run-dev-zbj.sh
@@ -0,0 +1,11 @@
+docker run --rm -it -h cm \
+ -p 8300:8300 \
+ -p 8301:8301 \
+ -p 8301:8301/udp \
+ -p 8302:8302 \
+ -p 8302:8302/udp \
+ -p 8400:8400 \
+ -p 8500:8500 \
+ -p 53:53/udp \
+ -v $(pwd)/etc/consul.d:/etc/consul.d \
+ progrium/consul -server -bootstrap -config-dir /etc/consul.d
