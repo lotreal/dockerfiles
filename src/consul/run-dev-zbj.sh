c4d1df3125756ebe46d6eb881013087f046d593c
Luo Tao <lotreal@gmail.com>
2015-01-29 22:33:32 +0800

    bind dns to ip

--- src/consul/run-dev-zbj.sh
+++ src/consul/run-dev-zbj.sh
@@ -1,4 +1,4 @@
-docker run -d --name consul -h master \
+docker run -d -h master \
  -p 8300:8300 \
  -p 8301:8301 \
  -p 8301:8301/udp \
@@ -6,6 +6,6 @@ docker run -d --name consul -h master \
  -p 8302:8302/udp \
  -p 8400:8400 \
  -p 8500:8500 \
- -p 53:53/udp \
+ -p 192.168.1.42:53:53/udp \
  -v $(pwd)/etc/consul.d:/etc/consul.d \
  progrium/consul -server -bootstrap -config-dir /etc/consul.d
